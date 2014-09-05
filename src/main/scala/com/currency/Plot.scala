fc0d4b0038f7b02aab4fa6dd17127eb388490dab
mjelnr@gmail.com <surgery2638>
2014-09-05 16:46:38 +0400

    05.09.2014

--- src/main/scala/com/currency/Plot.scala
+++ src/main/scala/com/currency/Plot.scala
@@ -1,7 +1,8 @@
 package com.currency
 
 import java.awt.Font
-import java.awt.event.{MouseWheelEvent, MouseWheelListener, MouseAdapter}
+import java.awt.event._
+import java.awt.geom.Rectangle2D
 import java.text.SimpleDateFormat
 
 import org.jfree.chart.{ChartPanel, JFreeChart}
@@ -16,7 +17,7 @@ import scala.swing._
  * Created by daniil.monakhov on 13.08.2014.
  */
 
-object Plot extends MainFrame with scala.swing.Reactor /*with App*/ {
+object Plot extends MainFrame with scala.swing.Reactor/* with App*/ {
   def show(title: String, list: List[CurrencyData]) {
       this.title = title;
       val legend: String = "Raiffeisen USD and EUR exchange currency rate by the date"
@@ -46,26 +47,67 @@ object Plot extends MainFrame with scala.swing.Reactor /*with App*/ {
       yAxis.setRange(35.0, 37.5)
 
       val renderer: XYItemRenderer = new XYLineAndShapeRenderer
-      val plot: XYPlot = new XYPlot(dataset,
-        xAxis, yAxis, renderer)
+      val plot: XYPlot = new XYPlot(dataset, xAxis, yAxis, renderer)
       val chart: JFreeChart = new JFreeChart(legend, new Font("Tahoma", Font.PLAIN, 18), plot, true)
-      val chartPanel: ChartPanel = new ChartPanel(chart)
+      val chartPanel: ChartPanelEx = new ChartPanelEx(chart)
+
 //      chartPanel.setPreferredSize(new Dimension(1280, 800))
       chartPanel.setPreferredSize(new Dimension(640, 480))
 
-      chartPanel.addMouseListener(new MouseAdapter {
-        override def mouseClicked(e: java.awt.event.MouseEvent) {
+//      chartPanel.getTopLevelAncestor.addKeyListener(new KeyAdapter {
+//        override def keyPressed(e: java.awt.event.KeyEvent) {
+//          println(" keyCode=" + e.getKeyCode + " keyChar=" + e.getKeyChar + " keyLocation=" + e.getKeyLocation + " keyExtCode=" + e.getExtendedKeyCode)
+//        }
+//      })
+
+//      chartPanel.addKeyListener(new KeyAdapter {
+//        override def keyPressed(e: java.awt.event.KeyEvent) {
+//          println(" keyCode=" + e.getKeyCode + " keyChar=" + e.getKeyChar + " keyLocation=" + e.getKeyLocation + " keyExtCode=" + e.getExtendedKeyCode)
+//        }
+//      })
+
+    val mouseAdapter = new MouseAdapter {
+      var x = 0
+      var y = 0
+
+      override def mouseDragged(e: java.awt.event.MouseEvent) {
+        if (e.getModifiers == 18) {
+            def xRangeValue =  (e.getX - x) / 10d
+            def yRangeValue =   (e.getY - y) / 10d
+            xAxis.setRange(xAxis.getRange.getLowerBound + xRangeValue, xAxis.getRange.getUpperBound + xRangeValue)
+            yAxis.setRange(yAxis.getRange.getLowerBound + yRangeValue, yAxis.getRange.getUpperBound + yRangeValue)
         }
-      })
+//        println(e.getButton + " mouse dragged (" + e.getX + ", " + e.getY +
+//            "); (" + e.getModifiers + " " + e.getModifiersEx + ")")
+      }
+
+      override def mousePressed(e: java.awt.event.MouseEvent) {
+        x = e.getX; y = e.getY
+        println(e.getButton + " mouse pressed (" + e.getModifiers + " " + e.getModifiersEx + ")")
+        val rect = chartPanel.getScreenDataArea(e.getX, e.getY)
+        val point = getPointInRectangle(e.getX, e.getY, rect)
+        val screenDataArea = chartPanel.getScreenDataArea(math.round(point._1).toInt, math.round(point._2).toInt)
+//        chartPanel.getPoint
+        println("rect: " + rect + " point: (" + point._1 + ", " + point._2 + ")")
+      }
+
+      def getPointInRectangle(x: Int, y: Int, area: Rectangle2D) = {
+        (math.max(math.ceil(area.getMinX), math.min(x, math.floor(area.getMaxX))),
+        math.max(math.ceil(area.getMinY), math.min(y, math.floor(area.getMaxY))))
+      }
+    }
+
+
+
+      chartPanel.addMouseMotionListener(mouseAdapter)
+      chartPanel.addMouseListener(mouseAdapter)
+
       chartPanel.addMouseWheelListener(new MouseWheelListener {
         override def mouseWheelMoved(e: MouseWheelEvent) {
-          println(" prWhRot:" + e.getPreciseWheelRotation + "  WhRot:" + e.getWheelRotation + " ScrT:" + e.getScrollType + " ScrAm:" + e.getScrollAmount
-          + " UnTScr:" + e.getUnitsToScroll)
-          def rangeValue =  e.getUnitsToScroll / 10d
+          def rangeValue =  -e.getUnitsToScroll / 10d
           yAxis.setRange(yAxis.getRange.getLowerBound + rangeValue, yAxis.getRange.getUpperBound + rangeValue)
         }
       })
-
       contents = new BorderPanel {
         peer.add(chartPanel)
       }
@@ -75,6 +117,28 @@ object Plot extends MainFrame with scala.swing.Reactor /*with App*/ {
 
   val lst = CurrencyData.parse("2014.08.13 35,60/36,85 47,55/49,20") ::
             CurrencyData.parse("2014-08-12 35,40/36,70 47,40/49,15") ::
-            CurrencyData.parse("2014-08-11 35,40/36,65 47,40/49,10") :: Nil
+            CurrencyData.parse("2014-08-11 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-08-10 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-08-09 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-08-08 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-08-07 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-08-06 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-08-05 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-08-04 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-08-03 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-08-02 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-08-01 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-07-31 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-07-30 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-07-29 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-07-28 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-07-27 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-07-26 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-07-25 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-07-24 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-07-23 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-07-22 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-07-21 35,40/36,70 47,40/49,15") ::
+            CurrencyData.parse("2014-07-20 35,40/36,65 47,40/49,10") :: Nil
   show("Raiffeisen currency rate Raiffeisen USD and EUR exchange currency rate by the date", lst)
 }
